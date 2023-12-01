14bb660075212894339be5a530291b53b6e65407
Cristian Pisco <cristian.pisco@outlook.com>
2023-12-01 12:38:03 -0500

    cambios al llamar el metodo CORS

--- docker-3/backend/app.py
+++ docker-3/backend/app.py
@@ -3,7 +3,8 @@ from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, origins="*")
+# CORS(app, origins="*")
+CORS(app)
 
 @app.route('/api/getMyInfo')
 def getMyInfo():
